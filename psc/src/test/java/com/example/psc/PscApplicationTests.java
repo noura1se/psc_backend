6af54e6dc587014f729df5f337b2bfbab6233102
noura1se <mssaouinoura@gmail.com>
2025-02-07 13:16:39 +0100

    inital commit

---
+++ psc/src/test/java/com/example/psc/PscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
