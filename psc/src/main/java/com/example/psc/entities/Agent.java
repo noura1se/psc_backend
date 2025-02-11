0570629d082d98bf283cccab0062a04534363ec9
Niama-ES <developerzone91@gmail.com>
2025-02-11 20:08:31 +0100

    5

---
+++ psc/src/main/java/com/example/psc/entities/Agent.java
@@ -0,0 +1,41 @@
+package com.example.psc.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Agent extends User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long agentID;
+
+    private String typeAgt;
+
+    //constructor:
+
+    public Agent(Long agentID, String typeAgt) {
+        this.agentID = agentID;
+        this.typeAgt = typeAgt;
+    }
+
+    //getters and setters:
+
+    public Long getAgentID() {
+        return agentID;
+    }
+
+    public void setAgentID(Long agentID) {
+        this.agentID = agentID;
+    }
+
+    public String getTypeAgt() {
+        return typeAgt;
+    }
+
+    public void setTypeAgt(String typeAgt) {
+        this.typeAgt = typeAgt;
+    }
+}
--- psc/src/main/java/com/example/psc/entities/User.java
+++ psc/src/main/java/com/example/psc/entities/User.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
@@ -13,8 +15,84 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long user_id;
+    private Long userID;
 
-    private String first_name;
-    private String
+    //parameters:
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+    private String email;
+    private String password;
+    private String role;
+
+    //constructor:
+    public User() {
+    }
+
+    public User(Long userID, String firstName, String lastName, LocalDate dob, String email, String password, String role) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    //getters and setters:
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
